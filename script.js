9b3b20ef1826f36d9efbe47fb116608d0fe8cbff
Abdul Awul Arman <abdulawularman52114@gmail.com>
2026-03-12 15:24:19 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+    // gsap.to("#box",{
+    //     x:1000,
+        
+    //     duration:2,
+    //     delay:1,
+        
+        
+    // })
+    // gsap.from("#box2",{
+    //     x:1000,
+    //     duration:2,
+    //     delay:1,
+            
+        
+    // })
+
+    // gsap.from("h1",{
+    //     y:50,
+    //     duration:1,
+    //     delay:1,
+    //     opacity:0,
+    //     stagger:-1,
+    // })
+
+    WebGLSampler.
