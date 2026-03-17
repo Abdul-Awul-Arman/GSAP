3ac63d8ab011f272abbf9f851da94a57159534f6
Abdul Awul Arman <abdulawularman52114@gmail.com>
2026-03-17 21:05:34 +0600

    Update Basic Project hero section, CSS styles, and GSAP animations

--- Basic Project/script.js
+++ Basic Project/script.js
@@ -1,23 +1,49 @@
+ function page1Animation(){
 
-let tl = gsap.timeline();
+     let tl = gsap.timeline();
 
-// 1. First animation: Logo
-tl.from(".logo,button", {
-    y: -30,
-    duration: 0.5,
-    opacity: 0
-})
+     // 1. First animation: Logo
+     tl.from(".logo,nav button", {
+         y: -30,
+         duration: 0.2,
+         opacity: 0
+     })
 
-// 2. Starts immediately after the first one (logo)
-tl.from("nav a", {
-    y: -30,
-    duration: 0.5,
-    opacity: 0,
-    ease: "power2.out",
-    stagger: {
-        each: 0.2,
-        from: "edges" 
-    }
-}, "-=0.2") // Position parameter: overlap it slightly with the logo animation
+     // 2. Starts immediately after the first one (logo)
+     tl.from("nav a", {
+         y: -30,
+         duration: 0.2,
+         opacity: 0,
+         ease: "power2.out",
+         stagger: {
+             each: 0.2,
+             from: "edges"
+         }
+     }, "-=0.2") // Position parameter: overlap it slightly with the logo animation
 
-// 3. Finally, animate the button
+     // 3. Finally, animate the button
+
+     tl.from('.hero-text', {
+         x: -500,
+         duration: .3,
+         opacity: 0,
+         ease: "power2.out"
+     })
+
+     tl.from('.hero-image', {
+         y: 500,
+         duration: .3,
+         opacity: 0,
+         ease: "power2.out"
+     })
+
+     tl.from('.logo-text', {
+         y: 30,
+         duration: .6,
+         opacity: 0,
+         stagger: .15
+     })
+
+    
+ }
+ page1Animation();
